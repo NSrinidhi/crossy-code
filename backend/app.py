05b51adf9dcc517efacb876e5701746081a6cc22
Srinidhi Narayanan <srinidhi@Srinidhis-MacBook-Air.local>
2024-09-15 05:17:11 -0400

    even more backend rahhhh

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import os
 import openai
 from openai import OpenAI
 import apikey from apikey.py
+import subprocess
 
 app = Flask(__name__)
 
@@ -10,13 +11,14 @@ app = Flask(__name__)
 def home():
 	return "Hello, world!"
 
-# given theme -> return words
-# given words -> return grid
+# given theme -> get words --> return grid (helper for grid-to-clue and mega-dictionary)
 # given grid -> return clues
 # given filled grid -> return correctness
 
-@app.route('/get-words')
-def get_words(theme):
+#@app.route('/get-grid')
+def get_grid(theme):
+
+	#start by getting superset of 200 relevant words
 	openai.api_key = apikey
 	client = OpenAI(api_key=openai.api_key)
 
@@ -37,18 +39,52 @@ def get_words(theme):
 	raw_output = test_chat.choices[0].message.content
 
 	exec(raw_output)
-	return words
+	#return words
 
-@app.route('/get-grid')
-def get_grid():
-	return "Hello, world!"
+	#words is the list of potential words
+
+	#make .txt file with these words (scored as 1s) and remaining dictionary words (scored as 0s)
+	with open('fullwordlist.txt', 'w') as file:
+		for word in words:
+			file.write(f"{word}; 1\n")
+
+		#change scores of all words in spread_the_word_list to be 0
+		spread_the_word_lines = []
+		with open('spreadthewordlist.txt', 'r') as file:
+			for line in file:
+				word, _ = line.split(';')
+				if word not in words: #check that we don't ovewrite the superset words
+					spread_the_word_lines.append(f"{word.strip()}; 0\n")
+		
+		#fullwordlist.txt is the full .txt file
+		file.writelines(spread_the_word_lines)
+	
+	for i in range(1, 31):
+    	grid_filename = f'grids/g{i}.txt'
+    	v = subprocess.run(['ingrid_core', grid_filename, '--wordlist', 'fullwordlist.txt', '--min-score', '3'], encoding="utf-8", capture_output=True)
+		if v.returncode == 0:
+			break
+	if v.returncode == 0:
+		return v.stdout
+	else:
+		return None
+
 
 @app.route('/get-words')
-def get_words(grid):
-	grid_arr = []
-	with open(filename) as file:
-		while line := file.readline():
-			grid_arr.append(list(line.rstrip()))
+def get_words(theme):
+
+	#get grid from theme first
+	grid = get_grid(theme)
+	if grid is None:
+		return {'Error': True}
+
+	#given a grid from a particular theme
+	grid_arr = grid.split("\n")
+	grid_arr = [list(row) for row in grid_arr]
+	#with open(filename) as file:
+	#	while line := file.readline():
+	#		grid_arr.append(list(line.rstrip()))
+
 	rows = len(grid_arr)
 	cols = len(grid_arr[0])
 	words = {}
@@ -97,10 +133,8 @@ def get_clues(words):
 		exec(clue_output)
 		word_to_clue[word] = clue_output
 
+	word_to_clue['Error'] = False
 	return word_to_clue
 
-@app.route('/check-grid')
-def check_grid():
-	return "Hello, world!"
 
 app.run(port=5000)
